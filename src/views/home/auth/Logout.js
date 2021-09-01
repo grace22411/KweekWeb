34d8baeb956637ef28da169d577325b14349e0f5
Adebowale Adebusuyi <amadebusuyi@gmail.com>
2021-09-01 08:36:07 +0100

    Update to dashboard

--- src/views/home/auth/Logout.js
+++ src/views/home/auth/Logout.js
@@ -3,6 +3,14 @@ import Authentication from '../../../Authentication';
 
 export const LogoutView = () => {
   Authentication.signOut();
+  return (
+    <>
+      <p style={{fontSize: 20, position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
+        <i className="fas fa-spinner fa-spin"></i> &nbsp;
+        <span>Signing out...</span>
+      </p>
+    </>
+  )
 }
 
 export default LogoutView;
